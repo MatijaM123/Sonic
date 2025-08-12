db6493e95ab1dd1439bc3caa8a8abd740502bb61
Matija Marinković <bombmatija@gmail.com>
2025-08-12 17:27:24 +0200

    Napisan bazničan lexer za jezik, koristi python biblioteku re kako bi pomoću regularnih izraza pronašao tokene koje ću kasnije koristiti za parsiranje koda.

---
+++ lexer.py
@@ -0,0 +1,59 @@
+import re
+
+class LexerError(Exception):
+    pass
+
+class Token:
+    def __init__(self, type_, value, line, col):
+        self.type = type_
+        self.value = value
+        self.line = line
+        self.col = col
+    def __repr__(self):
+        return f"Token({self.type}, {self.value}, {self.line}, {self.col})"
+
+class Lexer:
+    def __init__(self, code):
+        self.code = code
+        self.keywords = {'def', 'return', 'if', 'else', 'elif', 'while', 'for', 'in', 'void'}
+        self.token_specification = [
+            ('ARROW',    r'->'),
+            ('COMPARE',  r'(==|!=|<=|>=|<|>)'),
+            ('ASSIGN',   r'='),
+            ('OP',       r'[+\-*/]'),
+            ('NUMBER',   r'\d+(\.\d*)?'),
+            ('ID',       r'[A-Za-z_][A-Za-z_0-9]*'),
+            ('NEWLINE',  r'\n'),
+            ('SKIP',     r'[ \t]+'),
+            ('COLON',    r':'),
+            ('COMMA',    r','),
+            ('LPAREN',   r'\('),
+            ('RPAREN',   r'\)'),
+            ('LBRACE',   r'\{'),
+            ('RBRACE',   r'\}'),
+            ('LSQUARE',  r'\['),
+            ('RSQUARE',  r'\]'),
+            ('COMMENT',  r'\#.*'),
+            ('UNKNOWN',  r'.'),
+        ]
+        self.tok_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in self.token_specification)
+
+    def generate_tokens(self):
+        line_num = 1
+        line_start = 0
+        for mo in re.finditer(self.tok_regex, self.code):
+            kind = mo.lastgroup
+            value = mo.group()
+            column = mo.start() - line_start + 1
+            if kind == 'NEWLINE':
+                line_num += 1
+                line_start = mo.end()
+                yield Token('NEWLINE', value, line_num-1, column)
+            elif kind == 'SKIP' or kind == 'COMMENT':
+                pass
+            elif kind == 'ID' and value in self.keywords:
+                yield Token(value.upper(), value, line_num, column)
+            elif kind == 'UNKNOWN':
+                raise LexerError(f"Unknown token '{value}' at line {line_num} col {column}")
+            else:
+                yield Token(kind, value, line_num, column)
